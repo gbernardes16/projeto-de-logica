835ec995760ccdd75d652802cade8fbe812d02d6
Guilherme Bernardes Camilo <guibcdf@gmial.com>
2025-07-27 12:13:27 -0300

    projeto de lógica

---
+++ 3411-praticando-logica-main/ingresso/js/app.js
@@ -0,0 +1,11 @@
+let friends = ['abada' , 'jj']
+let amigos = ['Guilherme', 'João', 'Maria', 'Ana'];
+function comprar(array, a){
+    if (array.includes(a)){
+        console.log('ja ta doido');
+    } else {
+        console.log('nao ta doido');
+    }
+}
+
+comprar(friends, 'jkj');
